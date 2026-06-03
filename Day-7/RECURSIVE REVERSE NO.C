e741b99ffbe9a6513e3fa69834a49bcdafdf5e8c
shall-bal <mohdahshamarshan@gmail.com>
2026-06-03 17:26:00 +0530

    Day-7 uploaded

---
+++ Day-7/RECURSIVE REVERSE NO.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int rev = 0;
+
+void reverse(int n) {
+    if(n == 0)
+        return;
+
+    rev = rev * 10 + n % 10;
+    reverse(n / 10);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    reverse(n);
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
