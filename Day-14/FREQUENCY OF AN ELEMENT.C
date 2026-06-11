170ed752af84d7718f4865904369fd085c547b08
shall-bal <mohdahshamarshan@gmail.com>
2026-06-11 18:13:31 +0530

    Add C program to count frequency of an element

---
+++ Day-14/FREQUENCY OF AN ELEMENT.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, key, count = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(a[i] == key)
+            count++;
+    }
+
+    printf("Frequency = %d", count);
+
+    return 0;
+}
