c64946c7d763dce0d141c886bab473231c1d8dd8
shall-bal <mohdahshamarshan@gmail.com>
2026-06-11 18:14:43 +0530

    Add C program to find duplicates in an array
    Implemented a program to find and print duplicate elements in an array.

---
+++ Day-14/DUPLICATES IN ARRAY.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Duplicates: ");
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
